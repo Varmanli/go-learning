011a3350f8f655c03d9db885cc3f85564c1e9fa6
varmanli <varmanliamirhosein@gmail.com>
2026-09-17 20:09:06 +0330

    Learning Conditionals

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
 func main() {
-	
+	//if
+	//age := 20
+	//hasTicket := false
+	//if age >= 18 && hasTicket {
+	//	fmt.Println("You can enter")
+	//} else {
+	//	fmt.Println("You cannot enter")
+	//}
+	//if age := 17; age >= 18 {
+	//	fmt.Println("Adult")
+	//} else {
+	//	fmt.Println("Minor")
+	//}
+
+	//switch
+	//month := "March"
+	//
+	//switch month {
+	//case "December", "January", "February":
+	//	fmt.Println("Winter")
+	//case "March", "April", "May":
+	//	fmt.Println("Spring")
+	//case "June", "July", "August":
+	//	fmt.Println("Summer")
+	//case "September", "October", "November":
+	//	fmt.Println("Autumn")
+	//default:
+	//}
 }
