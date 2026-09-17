024939913f27ff6017027ab941c119bf742ba9aa
varmanli <varmanliamirhosein@gmail.com>
2026-09-17 21:10:11 +0330

    Learn For Loop

--- main.go
+++ main.go
@@ -29,4 +29,64 @@ func main() {
 	//	fmt.Println("Autumn")
 	//default:
 	//}
+
+	//for
+
+	//for i := 0; i < 5000; i++ {
+	//	fmt.Println(i)
+	//}
+	//for initialization; condition; post {
+	// code
+	//}
+	//for i := 1; i <= 5; i++ {
+	//	fmt.Println(i)
+	//}
+	//
+	//for i := 10; i >= 0; i -= 2 {
+	//	fmt.Println(i)
+	//}
+	//
+	//for i := 20; i > 0; i -= 5 {
+	//	fmt.Println(i)
+	//}
+	//
+	//for i := 1; i <= 10; i++ {
+	//	if i == 5 {
+	//		continue
+	//	}
+	//	fmt.Println(i)
+	//}
+	//languages := []string{"Go", "Python", "Rust"}
+	//
+	//for index, value := range languages {
+	//	fmt.Println(index, ":", value)
+	//}
+	//languages := []string{"Go", "Python", "Rust"}
+	//
+	//for _, language := range languages {
+	//	fmt.Println(language)
+	//}
+
+	//languages := map[string]int{
+	//	"Go":     10,
+	//	"Python": 8,
+	//	"Rust":   9,
+	//}
+	//
+	//for lang, score := range languages {
+	//	fmt.Println(lang, score)
+	//}
+	//
+	//for lang := range languages {
+	//	fmt.Println(lang)
+	//}
+	//
+	//for _, score := range languages {
+	//	fmt.Println(score)
+	//}
+	//text := "A🔥Go"
+	//
+	//for index, r := range text {
+	//	fmt.Printf("%d -> %c\n", index, r)
+	//}
 }
